85f95569cec6858d288ea531bb3ea319df4089a0
W0823298 <W0823298@ADL-A1201I-11>
2023-10-13 13:33:29 -0400

    I aaded a folder Data and created a file under it and along with it i updated buttons and in startup file i added some using statements

---
+++ MvcButton/Data/MvcButtonContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcButton.Models;
+
+namespace MvcButton.Data
+{
+    public class MvcButtonContext : DbContext
+    {
+        public MvcButtonContext(DbContextOptions<MvcButtonContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Button> Button { get; set; }
+    }
+}
--- MvcButton/Models/Button.cs
+++ MvcButton/Models/Button.cs
@@ -1,11 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcButton.Models
 {
     public class Button
     {
+        public int Id { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Colour { get; set; }
+
+        public string Shape { get; set; }
+
+        public decimal Price { get; set; }
     }
 }
